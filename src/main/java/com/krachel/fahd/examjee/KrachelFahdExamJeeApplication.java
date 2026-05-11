a6fa9a380fe055c5946d25b1c885ff8ac80e9346
FahD <afkarfhd@gmail.com>
2026-05-11 09:40:37 +0000

    structure du projet

---
+++ src/main/java/com/krachel/fahd/examjee/KrachelFahdExamJeeApplication.java
@@ -0,0 +1,12 @@
+package com.krachel.fahd.examjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrachelFahdExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrachelFahdExamJeeApplication.class, args);
+    }
+}
